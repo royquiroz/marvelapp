2c34cf66945f7ae0536e871c61302d2703c6a25b
Gonzalo Quiroz <kiyegoo@gmail.com>
2018-05-31 21:52:27 -0500

    Se agrega pagina para mostrar personajes (en desarrollo)

--- src/app/components/character/character.component.ts
+++ src/app/components/character/character.component.ts
@@ -1,27 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { MarvelService } from '../../services/marvel.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { MarvelService } from "../../services/marvel.service";
 
 @Component({
-  selector: 'app-character',
-  templateUrl: './character.component.html',
+  selector: "app-character",
+  templateUrl: "./character.component.html",
   styles: []
 })
 export class CharacterComponent {
+  character: any = {};
 
-  constructor( private marvel: MarvelService, private router: ActivatedRoute ) {
-
-    this.router.params.subscribe( params => {
-      this.getCharacterForId( params['id'] )
-    })
-    
+  constructor(private marvel: MarvelService, private router: ActivatedRoute) {
+    this.router.params.subscribe(params => {
+      this.getCharacterForId(params["id"]);
+    });
   }
-  
+
   getCharacterForId(id: number) {
-    this.marvel.getCharacterForId(id).subscribe( (data: any) => {
+    this.marvel.getCharacterForId(id).subscribe((data: any) => {
       console.log(data);
-    })
-   }
-
-
+      this.character = data.data.results[0];
+    });
+  }
 }
